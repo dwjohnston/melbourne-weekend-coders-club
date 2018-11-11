f177fe73d76c134925eedbe5f535fe91921715fb
David Johnston <david@blacksheepcode.com>
2018-11-11 16:47:31 +1100

    Got this thing sorted!

--- src/shared/components/Home.js
+++ src/shared/components/Home.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import { Link } from 'react-router-dom';
 
-export default class Home extends Component {
-    render() {
-        return (
+import { addTodo } from '../actions/todos';
+
+const mapStateToProps = ({ todos }) => ({
+    todos
+});
+
+@connect(mapStateToProps, {
+    addTodo
+})
+class Home extends Component {
+
+
+  handleAddTodoClick = () => {
+      this.props.addTodo(`Random Todo #${Math.round(Math.random() * 100)}`);
+  };
+
+  render() {
+
+      const { todos } = this.props;
+
+      return (
           <div>
-            <h1>Welcome to React Fiber.</h1>
-            <ul>
-              <li>
-                <Link to='/about'>About</Link>
-              </li>
-              <li>
-                <Link to='/random-link'>Not Found</Link>
-              </li>
-            </ul>
+              <h1>Welcome to React Fiber.</h1>
+
+              <ul>
+                  {todos.map(todo =>
+                      <li key={todo.id}>{todo.name}</li>
+                  )}
+              </ul>
+              <button onClick={this.handleAddTodoClick}>Add random todo</button>
+
+              <ul>
+                  <li>
+                      <Link to='/about'>About</Link>
+                  </li>
+                  <li>
+                      <Link to='/random-link'>Not Found</Link>
+                  </li>
+              </ul>
           </div>
-        );
-    }
+      );
+  }
 
 }
+
+export default Home; 
